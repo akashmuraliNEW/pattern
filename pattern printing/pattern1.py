5f8c5e3bdc9a4ee008d678f8552e804ec6a7b0d8
AKASH KM <akashunni869@gmail.com>
2024-11-25 23:13:06 +0530

    s

--- pattern printing/pattern1.py
+++ pattern printing/pattern1.py
@@ -526,15 +526,15 @@
     #    P Q R S T U  
     #   V W X Y Z [ \  
 
-size = 7
-asciiNumber = 65
-m = (2 * size) - 2
-for i in range(0, size):
-    for j in range(0, m):
-        print(end=" ")
-    m = m - 1
-    for j in range(0, i + 1):
-        character = chr(asciiNumber)
-        print(character, end=' ')
-        asciiNumber += 1
-    print(" ")
+# size = 7
+# asciiNumber = 65
+# m = (2 * size) - 2
+# for i in range(0, size):
+#     for j in range(0, m):
+#         print(end=" ")
+#     m = m - 1
+#     for j in range(0, i + 1):
+#         character = chr(asciiNumber)
+#         print(character, end=' ')
+#         asciiNumber += 1
+#     print(" ")
