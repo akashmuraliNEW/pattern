00595ac3e64226d30f62d91d3a595bd3d26d4e99
AKASH KM <akashunni869@gmail.com>
2024-11-26 12:10:59 +0530

    begin to add new patterns in new files instead of single file

--- pattern printing/patterns_FULL.py
+++ pattern printing/patterns_FULL.py
@@ -538,3 +538,23 @@
 #         print(character, end=' ')
 #         asciiNumber += 1
 #     print(" ")
+
+    #         A  
+    #        B C  
+    #       D E F  
+    #      G H I J  
+    #     K L M N O  
+    #    P Q R S T U  
+    #   V W X Y Z [ \  
+row =7
+col=1
+char_num = 65
+for i in range(row,0,-1):
+    for j in range(i):
+        print(' ',end='')
+    for k in range(col):
+        print(chr(char_num),end=' ')
+        char_num+=1
+    print()
+    
+    col+=1
