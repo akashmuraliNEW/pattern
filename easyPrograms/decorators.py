5a1eb865aab2cc190839ae7519912ebb47699486
AKASH KM <akashunni869@gmail.com>
2024-12-11 10:33:40 +0530

    decorators args

--- easyPrograms/decorators.py
+++ easyPrograms/decorators.py
@@ -1,25 +1,41 @@
 def upperCase(func_text):
-    def wrapper(): 
-        result = func_text()
+    def wrapper(*arg): 
+        result = func_text(*arg)
         return result.upper()
 
     return wrapper
 
 
-def lowerCase(func_text):
-    def wrapper():
-        result = func_text()
-        # print('started')
+# def lowerCase(func_text):
+#     def wrapper():
+#         result = func_text()
+#         # print('started')
 
-        return result.lower()
+#         return result.lower()
+#     return wrapper
+
+# @lowerCase
+# def lower_text():
+#     return 'HeY mY namE Is AK'
+# @upperCase
+# def upper_text():
+#     return 'HeY mY namE Is AK'
+
+# print(lower_text())
+# print(upper_text())
+
+
+# decorators with arguments
+
+
+def decorator(func):
+    def wrapper(*args):
+        result = func(*args)
+        return result
     return wrapper
-
-@lowerCase
-def lower_text():
-    return 'HeY mY namE Is AK'
+@decorator
 @upperCase
-def upper_text():
-    return 'HeY mY namE Is AK'
 
-print(lower_text())
-print(upper_text())
+def arg_text(name):
+    return f'hey my name is {name}'
+print(arg_text('akash'))
