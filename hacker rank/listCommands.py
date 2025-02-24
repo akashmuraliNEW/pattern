3b4d4d79291556eb640ef2e01c52c89ff9c64e80
AKASH KM <akashmurali013@gmail.com>
2025-02-24 12:45:17 +0530

    hackerrank

---
+++ hacker rank/listCommands.py
@@ -0,0 +1,13 @@
+N = int(input())
+dict1={
+    1:'insert',
+    2:'print',
+    3:'remove',
+    4:'append',
+    6:'sort',
+    7:'reverse'
+
+}
+for i in range(input()):
+    if dict1[N]=='insert':
+        pass
