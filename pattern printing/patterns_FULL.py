70f8afe8ae8d6d609618746876f4dbe678453a8b
AKASH KM <akashmurali013@gmail.com>
2025-02-28 10:42:55 +0530

    patternPrint

--- pattern printing/patterns_FULL.py
+++ pattern printing/patterns_FULL.py
@@ -546,15 +546,15 @@
     #     K L M N O  
     #    P Q R S T U  
     #   V W X Y Z [ \  
-row =7
-col=1
-char_num = 65
-for i in range(row,0,-1):
-    for j in range(i):
-        print(' ',end='')
-    for k in range(col):
-        print(chr(char_num),end=' ')
-        char_num+=1
-    print()
+# row =7
+# col=1
+# char_num = 65
+# for i in range(row,0,-1):
+#     for j in range(i):
+#         print(' ',end='')
+#     for k in range(col):
+#         print(chr(char_num),end=' ')
+#         char_num+=1
+#     print()
     
-    col+=1
+#     col+=1
