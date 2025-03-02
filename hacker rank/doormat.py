14e930fca4a28532585ad13c8f324cf7d22d6728
AKASH KM <akashmurali013@gmail.com>
2025-03-02 22:05:27 +0530

    hackerrank

---
+++ hacker rank/doormat.py
@@ -0,0 +1,13 @@
+input = input("")
+input= input.split()
+N= int(input[0])
+M= int(input[1])
+patron=".|."
+for i in range (0, int((N)/2)):
+    numRepet = (i+1+i)
+    print((numRepet*patron).center(M,"-"))
+print("WELCOME".center(M,"-"))
+for k in range(int((N)/2),0,-1):
+    numRepet = (k-1+k)
+    print ((numRepet*patron).center(M,"-"))
+    
