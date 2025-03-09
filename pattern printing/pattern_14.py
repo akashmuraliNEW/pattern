9d9b935ba4a4c60dc53b7f2b3b78fd4e51dd37de
AKASH KM <akashmurali013@gmail.com>
2025-03-09 23:07:58 +0530

    pattern

--- pattern printing/pattern_14.py
+++ pattern printing/pattern_14.py
@@ -4,7 +4,10 @@
 # 10 8 6 4 
 # 10 8 6 4 2
 row = 6
-num = 10
+
 for i in range(1,row):
+    num = 10
     for j in range(i):
-        print(num,end='')
+        print(num,end=' ')
+        num-=2
+    print()
