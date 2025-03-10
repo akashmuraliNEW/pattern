5f053242d399c3dbf8b4f915a037b9fd7c355e89
AKASH KM <akashmurali013@gmail.com>
2025-03-10 12:26:16 +0530

    pattern

--- pattern printing/pattern_16.py
+++ pattern printing/pattern_16.py
@@ -4,11 +4,12 @@
 # 4 5 
 # 5
 row = 6
-
+col = 5
 for i in range(1,row):
     num = i
-    for j in range(i):
+    for j in range(col):
         print(num,end=' ')
         num+=1
     print()
+    col-=1
 
