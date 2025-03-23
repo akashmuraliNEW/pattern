7f7d0f7eb07bc81cb2587aadff4b2eba07357106
AKASH KM <akashmurali013@gmail.com>
2025-03-23 22:22:25 +0530

    hackerrank

--- hacker rank/shoeShop.py
+++ hacker rank/shoeShop.py
@@ -7,7 +7,9 @@ dict1 = {}
 while customers>0:
     user_input = input("Enter key and value: ")  # Example: "6 55"
 
-    
+    key, value = map(int, user_input.split())
+    dict1[key]=value
+    customers-=1
 sum = 0
 for i in dict1:
     sum+=dict1[i]
