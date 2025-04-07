c2a969f2eb836f6422206a108134eefbce8f5e58
AKASH KM <akashmurali013@gmail.com>
2025-04-07 22:07:21 +0530

    hackerrank

--- hacker rank/spoitfy_dl.py
+++ hacker rank/spoitfy_dl.py
@@ -13,8 +13,4 @@ songs = spotdl.search([spotify_url])  # Search for the song
 result = spotdl.download_songs(songs)  # Download it
 
 # Print the result
-for song, path in result:
-    if path:
-        print(f"Downloaded '{song.display_name}' to {path}")
-    else:
-        print(f"Failed to download
+f
