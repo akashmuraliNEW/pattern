a3fdc30e94d86d3f4c475b9b4823d4e1d7c3da1e
AKASH KM <akashmurali013@gmail.com>
2025-04-15 00:00:09 +0530

    hackerrank

--- pattern printing/pattern_3.py
+++ pattern printing/pattern_3.py
@@ -4,11 +4,4 @@
 #   * * * * 
 # * * * * * 
 rows = 5
-col=1
-for i in range(rows,0,-1):
-    for j in range(i):
-        print(' ',end='')
-    for k in range(col):
-        print('*',end='')
-    col+=1
-    print()
+
