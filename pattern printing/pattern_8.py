05b5ad154876fc52d2dab2921feb8bf516943cfe
AKASH KM <akashmurali013@gmail.com>
2025-04-19 22:07:09 +0530

    pattern

--- pattern printing/pattern_8.py
+++ pattern printing/pattern_8.py
@@ -4,11 +4,3 @@
 # 4 4 4 4 5 
 # 5 5 5 5 5
 
-row = 5
-for i in range(row):
-    for j in range(1,row+1):
-        if i>=j:
-            print(i,end='')
-        else:
-         print(j,end='')
-    print()
