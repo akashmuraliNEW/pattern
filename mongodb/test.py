5fd2df150f2b9466efa76f2de5c1cb55c9a89001
AkashMuraliNEW <akashmurali013@gmail.com>
2025-05-02 13:55:56 +0530

    testing mongodb

--- mongodb/test.py
+++ mongodb/test.py
@@ -1,14 +1,23 @@
 from pymongo import MongoClient
 
 # Connect to your MongoDB (replace with your URI)
-client = MongoClient("mongodb+srv://telegram:telegramfiles@cluster0.piabe.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0")  # Replace with your URI if different
+client = MongoClient("mongodb://botuser:botpass@104.248.25.169:27017")
 
 # List all databases
-# db_names = client.list_database_names()
-# def check_db_stats():
-#     for db_name in db_names:
-#         db = client[db_name]
-#         print(f"\nStats for Database: {db_name}")
+db_names = client.list_database_names()
+
+def check_db_stats():
+    for db_name in db_names:
+        print(f"\nStats for Database: {db_name}")
+        db_name = client.list_database_names()
+        # db = client[db_name]
+        # stats = db.command("dbstats")
+        # for key, value in stats.items():
+        #     print(f"{key}: {value}")
+
+# Call the function
+check_db_stats()
+
         
 #         # Get stats for the database
 #         db_stats = db.command("dbStats")
@@ -32,14 +41,14 @@ client = MongoClient("mongodb+srv://telegram:telegramfiles@cluster0.piabe.mongod
 #     check_db_stats()
 
 
-old_db = client['telegram']
-old_collection = old_db['users']
-users = old_collection.find()
+# old_db = client['telegram']
+# old_collection = old_db['users']
+# users = old_collection.find()
 # Connect to new MongoDB instance
 # new_client = MongoClient("mongodb://new_user:new_password@new_mongo_url:27017/new_db_name")
 # new_db = new_client['new_db_name']
 # new_collection = new_db['users']  # Replace 'users' with your actual collection name
-j=0
+# j=0
 # for i in users:
 #     print(i)
 #     print('''''''''''''''''''''''''''''''''''''''''''''''')
@@ -47,3 +56,30 @@ j=0
 #         break
 #     j+=1
     
+# gfhfhfgfhfhfg
+
+oldb2 = 'mongodb+srv://telegram:telegramfiles@cluster0.piabe.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0'
+newdb2 = 'mongodb://botuser:botpass@104.248.25.169:27017/professorbot'
+
+
+# use admin
+# ... db.createUser({
+# ...   user: "telegram", 
+# ...   pwd: "telegram", 
+# ...   roles: [{ role: "userAdminAnyDatabase", db: "admin" }]
+# ... })
+# switched to db admin
+# admin> use professorbot
+# ... db.createUser({
+# ...   user: "botuser",
+# ...   pwd: "botpass",
+# ...   roles: [{ role: "readWrite", db: "professorbot" }]
+# ... })
+# ... 
+
+# db.updateUser("botuser", { pwd: "userpass" })
+# db.createUser({
+#   user: "botuser",
+#   pwd: "botpass",
+#   roles: [{ role: "readWrite", db: "professorbot" }]
+# })
